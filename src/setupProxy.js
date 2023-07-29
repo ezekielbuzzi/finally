f8b7054c9da775944189ecae6ad16821ec18a8b9
Hizkiel-Mesfin <mesfinezekiel@gmail.com>
2023-07-29 10:31:54 +0200

    target url in the proxy config changed

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = (app) => {
   app.use(
     ["/api"],
     createProxyMiddleware({
-      target: "http://localhost:5000",
+      target: "https://finally-api-production.up.railway.app/",
       changeOrigin: true,
     })
   );
